41d3150d415eeed9ac8250fa9f98cd69a13068a8
ranjit <ranjipatil368@gmail.com>
2024-07-18 11:41:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+const app = express();
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.get("/", (req, resp) => {
+    const courses = [
+        {
+            id: 1,
+            courseName: "MERN"
+        },
+        {
+            id: 2,
+            courseName: "MEAN"
+        },
+        {
+            id: 3,
+            courseName: "MERP"
+        }
+    ]
+    resp.send(courses)
+})
+
+app.listen(port, () => {
+    console.log(`server is running on http://${hostname}:${port}`)
+})
